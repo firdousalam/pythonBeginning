4bb454a1094763d3d54d52bb8798f86aa7ddf842
Firdous Alam <firdous.alam2058@gmail.com>
2026-02-01 21:30:28 +0530

    basic of python

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# need to import math modules for mathematical function
+import math
+
 print("hello world")
 print("*" * 10)
 print("hello world")
@@ -13,3 +16,103 @@ y = 3
 # to associate shortcut to run python by shortcut key
 # open command palette -> search for run python file -> clink om keybinding -> provide shortcut key and press enter
 unit_price = 3
+
+# funcdamental  to python
+student_count = 1000
+rating = 4.99
+is_published = False
+course_name = "python programming"
+print(student_count)
+# titple code to format long string
+message = """
+hi all
+this is long strong
+with multi line
+"""
+print(message)
+# length of sring
+lenght = len(message)
+print(f"Total length : {lenght}")
+# to get character
+print(message[1])
+print(message[-1])
+# slice a string print(message[0:3]) return first 3 character
+print(message[0:4])
+# hot to add double quoe in string -- we can use \
+course = " python \"programming"
+print(course)
+# escape sequences in python
+# \"
+# \'
+# \\
+# \n  for new line
+
+# to print full name
+
+first = "firdous"
+last = "alam"
+# full = first+" "+last
+# or we can use we can use f and {}
+full = f"{first} {last}"
+print(full)
+
+# string function in python
+print(course.capitalize())
+print(course.upper())
+print(course.title())
+print(course.strip())
+print(course)
+print(course.find("pro"))
+print(course.find("Pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)
+
+
+# number method there are 3 type of number integer,float and complex number (a+bj)
+
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)  # return only integer
+print(10 % 3)  # modulus
+print(10 ** 3)  # power
+
+print(round(2.9))
+
+print(abs(-2.9))
+# for mathematical function we need to import math modules
+
+print(math.ceil(2.2))
+
+# x = input("x: ")
+# Traceback (most recent call last):
+#  File "C:\Users\techn\TechnophileFirdous\pythonBeginning\app.py", line 90, in <module>
+#    y = x + 1
+#       ~~^~~
+# TypeError: can only concatenate str (not "int") to str
+# y = x + 1
+# print(type(x))
+# need to type convert
+# int(x)
+# float(x)
+# bool(x)
+# str(x)
+
+#
+
+# falsy
+# ""
+# 0
+# None
+# anyhting else is true
+
+# bool(0)
+# # False
+# bool(1)
+# # True
+# bool(-1)
+# # True
+# bool("")
+# # False
