528adcb53323cb44df29f09f36871785c983c3d3
Firdous Alam <firdous.alam2058@gmail.com>
2026-02-01 11:20:33 +0530

    adding extension in python

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 print("hello world")
 print("*" * 10)
+print("hello world")
+x = 1
+y = 3
+
+# we need to install python extention to usevscode as ide in python
+# you can run current file by clicking play button on top right
+# to format python
+# go to view -> commant palette search format document you need to install autopep8
+# run run format document and it will format your document
+# to format document on save goto file -> preferencs -> setting search and select  Format On Save now your code will get format automatically on save
+# to associate shortcut to run python by shortcut key
+# open command palette -> search for run python file -> clink om keybinding -> provide shortcut key and press enter
+unit_price = 3
